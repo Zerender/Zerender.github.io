3df661eb81e19e94f75c2980d2fe3cb306f78404
Kevin Guo <kev.guo123@gmail.com>
2021-06-10 01:02:07 -0500

    more tweaks

--- src/@lekoarts/gatsby-theme-cara/components/about.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/about.tsx
@@ -20,10 +20,10 @@ const About = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
     <Divider speed={0.1} offset={offset} factor={factor}>
       <UpDown>
         <SVG icon="triangle" width={8} stroke color="icon_darkest" left="25%" top="5%" />
-        <SVG icon="upDown" hiddenMobile width={24} color="icon_orange" left="80%" top="80%" />
+        <SVG icon="hexa" stroke hiddenMobile width={24} color="icon_orange" left="80%" top="80%" />
       </UpDown>
       <UpDownWide>
-        <SVG icon="arrowUp" hiddenMobile width={16} color="icon_purple" left="5%" top="80%" />
+        <SVG icon="hexa" stroke hiddenMobile width={16} color="icon_purple" left="5%" top="80%" />
         <SVG icon="triangle" width={12} stroke color="icon_brightest" left="95%" top="50%" />
         <SVG icon="circle" hiddenMobile width={6} color="icon_brightest" left="85%" top="15%" />
       </UpDownWide>
--- src/@lekoarts/gatsby-theme-cara/components/projects.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/projects.tsx
@@ -34,18 +34,18 @@ const Projects = ({ offset, factor }: { offset: number; factor?: number }) => (
     <Divider speed={0.1} offset={offset} factor={factor}>
       <UpDown>
         <SVG icon="box" width={6} color="icon_brightest" left="85%" top="75%" />
-        <SVG icon="upDown" width={8} color="icon_teal" left="70%" top="20%" />
+        <SVG icon="hexa" stroke width={8} color="icon_teal" left="70%" top="20%" />
       </UpDown>
       <UpDownWide>
-        <SVG icon="arrowUp" hiddenMobile width={16} color="icon_green" left="20%" top="90%" />
+        <SVG icon="hexa" stroke hiddenMobile width={16} color="icon_green" left="20%" top="90%" />
         <SVG icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="30%" />
         <SVG icon="circle" width={16} color="icon_yellow" left="70%" top="90%" />
         <SVG icon="triangle" hiddenMobile width={16} stroke color="icon_teal" left="18%" top="75%" />
         <SVG icon="circle" hiddenMobile width={6} color="icon_brightest" left="75%" top="10%" />
-        <SVG icon="upDown" hiddenMobile width={8} color="icon_green" left="45%" top="10%" />
+        <SVG icon="hexa" stroke hiddenMobile width={8} color="icon_green" left="45%" top="10%" />
       </UpDownWide>
       <SVG icon="circle" hiddenMobile width={6} color="icon_brightest" left="4%" top="20%" />
-      <SVG icon="circle" width={12} color="icon_pink" left="80%" top="60%" />
+      <SVG icon="circle" width={12} color="icon_purple" left="80%" top="60%" />
       <SVG icon="box" width={6} color="icon_orange" left="10%" top="10%" />
       <SVG icon="hexa" width={16} stroke color="icon_red" left="75%" top="30%" />
       <SVG icon="hexa" width={8} stroke color="icon_yellow" left="80%" top="70%" />
